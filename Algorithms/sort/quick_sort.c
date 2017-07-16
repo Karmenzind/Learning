a15f2710aa2eb74fdb5ad46ab93e5f5567a61ad2
Karmenzind <pi@raspberrypi>
2017-07-17 01:23:31 +0800

    correct quicksort

--- Algorithms/sort/quick_sort.c
+++ Algorithms/sort/quick_sort.c
@@ -12,35 +12,34 @@ int partition(int start, int end){
 	int l_idx, r_idx;
 
 	pivot = a[start];
-	l_idx = r_idx = 0;
-	for (i=start; i<end; i++){
+	l_idx = r_idx = -1;
+	for (i=start+1; i<=end; i++){
 		if (a[i] < pivot)
-		       left[l_idx++] = a[i];
+		       left[++l_idx] = a[i];
 		else
-			right[r_idx++] = a[i];
+			right[++r_idx] = a[i];
 	}
 	
-	if (sizeof(left) > 0){
 	for (i=0; i<=l_idx; i++)
-		a[start+i] = left[i];}
-	mid = start+l_idx+1+1;
+		a[start+i] = left[i];
+	mid = start + l_idx + 1;
 	a[mid] = pivot;
-	if (sizeof(right) > 0){
-	for (i=0; i<=r_idx; i++)
-		a[mid+i+1] = right[i];}
 
+	for (i=0; i<=r_idx; i++)
+		a[mid+i+1] = right[i];
+	
+	for (i=0; i<LEN; i++)
+		printf("%d ", a[i]);
+	printf("\n");
 	return mid;
 }
 
 void quicksort(int start, int end){
-	int mid;
+	int mid, i;
 	if (end > start){
 		mid = partition(start, end);
 		quicksort(start, mid-1);
 		quicksort(mid+1, end);
-		for (int i=0; i<LEN; i++)
-			printf("%d ", a[i]);
-		printf("\n");
 	}
 }
 
